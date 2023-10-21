06e3befa293adc0c41bdcad164f1e4bd55c82d19
Ellen Houghton <ellen.a.ash@gmail.com>
2023-10-21 15:51:54 +0100

    initially working following tutorial

---
+++ src/ScrabbleMain.java
@@ -0,0 +1,6 @@
+public class ScrabbleMain {
+    public static void main(String[] args) {
+        ScrabbleScore scorer = new ScrabbleScore();
+        System.out.println(scorer.getScrabbleScore("zoo"));
+    }
+}
