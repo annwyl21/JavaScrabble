236734ec9025fd19ef166321e6b729aa2c6e7fbc
Ellen Houghton <ellen.a.ash@gmail.com>
2023-10-21 16:09:45 +0100

    scorer working with - to exit

--- src/ScrabbleMain.java
+++ src/ScrabbleMain.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class ScrabbleMain {
     public static void main(String[] args) {
         ScrabbleScore scorer = new ScrabbleScore();
-        System.out.println(scorer.getScrabbleScore("zoo"));
+        System.out.println("Enter word or - to Quit:");
+        Scanner scanner = new Scanner(System.in); // new instance of Scanner class
+        String word = scanner.next();
+        while (!word.equals("-")) {
+            System.out.println(word + " Score: " + scorer.getScrabbleScore(word));
+            System.out.println("Enter word or - to Quit:");
+            word = scanner.next();
+
+        }
     }
 }
